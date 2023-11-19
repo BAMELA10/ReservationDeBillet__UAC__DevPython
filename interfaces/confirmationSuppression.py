1a2d1ae8a99ccc1b0f85b91c59523239b6577c1f
johan zo'o <zoojohan10@gmail.com>
2023-11-19 02:06:10 +0100

    new commit 1

--- interfaces/confirmationSuppression.py
+++ interfaces/confirmationSuppression.py
@@ -1,1 +1,14 @@
 #boite de confirmation de suppression
+import tkinter as Tk
+
+class mainProgramme:
+    def __init__(self, windows):
+        self.windows = windows
+        self.windows.title(" Home ")
+        self.windows.geometry("1024x720")
+        self.windows.resizable(False, False)
+        
+fenetre = Tk.Tk()
+fenetre1 = mainProgramme(fenetre)
+fenetre.mainloop()
+         
--- interfaces/reservation/historiqueReservation.py
+++ interfaces/reservation/historiqueReservation.py
@@ -1,1 +1,14 @@
 #fenetre d'historique de reservation
+import tkinter as Tk
+
+class mainProgramme:
+    def __init__(self, windows):
+        self.windows = windows
+        self.windows.title(" Home ")
+        self.windows.geometry("1024x720")
+        self.windows.resizable(False, False)
+        
+fenetre = Tk.Tk()
+fenetre1 = mainProgramme(fenetre)
+fenetre.mainloop()
+         
